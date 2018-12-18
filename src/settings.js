12f6fb9f51f023d8a89e7f6b6b248a1fac454696
KimDeokwon <codevillain@naver.com>
2018-12-19 01:18:32 +0900

    Update settings

---
+++ src/settings.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const settings = [{
+    // 소리 관련
+    'volume': {
+        'bgm': 1.0,
+        'voice': 1.0,
+        'sfx': 1.0
+    },
+
+    // 자동으로 진행
+    'auto': {
+        enable: true,
+        speed: 1.0
+    },
+
+    // 대사 출력 속도
+    'print-speed': 10,
+}];
+
+export default settings;
