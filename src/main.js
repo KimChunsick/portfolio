04df80731c49d9294d1419e53ef92cfffdad6172
KimDeokwon <codevillain@naver.com>
2018-12-19 01:19:09 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 "use strict";
 
+// var engine = require('./engine');
+import { Engine } from "./engine.js"
+import script from './scripts.js'
+import setting from './settings.js'
+
+console.log(script);
+console.log(setting);
+
+let engine = new Engine(script, setting);
+engine.start();
