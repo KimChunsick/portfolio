bc3556985fac464293f751d584bf9c40254cc13f
KimDeokwon <codevillain@naver.com>
2018-12-19 01:19:58 +0900

    Add character.js

---
+++ src/characters.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// 캐릭터 템플릿
+const character = {
+    name: '임시',
+    theme_color: '#ffffff',
+    state: {
+        // 필요한 상태 쭉쭉 추가
+        idle: '어.. 기본상태.png'
+    }
+};
+
+export default character;
