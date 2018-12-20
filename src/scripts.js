bb0c3fbfc3ba8dd1a1b75814dcc25e99aa788a29
KimDeokwon <codevillain@naver.com>
2018-12-21 00:54:34 +0900

    Update scripts.js
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- src/scripts.js
+++ src/scripts.js
@@ -2,13 +2,19 @@
 
 const test = [
     {'block': {'name': 'start'}},
+    {'bgm': {'filename': 'Zombie_March.mp3'}},
     {'talk': {'talker': '테스트', 'message': '테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.'}},
+    {'sfx': {'filename': 'shooting_star.mp3'}},
     {'talk': {'talker': '마을사람A', 'message': '지나가는중...'}},
     {'add': {'message': '   이건 추가다아아아아아아아아아아ㅏ아아아아아아앙'}},
-    {'choice': [{'to': 'start', 'message': '다시 처음으로'}, {'to': 'end', 'message': '이어서 ㄱ'}]},
+    {'choice': [{'message': '다시 처음으로', 'to': 'start'}, {'message': '이어서 ㄱ', 'to': 'end'}]},
     {'block': {'name': 'end'}},
     {'talk': {'talker': '', 'message': '근데 할말이 없네 다시 처음으로 ㄱ'}},
     {'jump': {'to': 'start'}}
 ];
 
-export default test;
+const test2 = [
+
+];
+
+export { test, test2 };
