0c491ed37e24243c9343fd8e218811f28823995f
KimDeokwon <codevillain@naver.com>
2018-12-20 22:07:41 +0900

    Update scripts.js
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,14 @@
 "use strict";
 
 const test = [
-    {'talk': {'talker': '테스트', 'message': '테슷희으이ㅡㅇ으ㅡㅇ응'}},
-    {'add': {'message': '   이건 추가다아아아아아아아아아아ㅏ아아아아아아앙'}}
+    {'block': {'name': 'start'}},
+    {'talk': {'talker': '테스트', 'message': '테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.'}},
+    {'talk': {'talker': '마을사람A', 'message': '지나가는중...'}},
+    {'add': {'message': '   이건 추가다아아아아아아아아아아ㅏ아아아아아아앙'}},
+    {'choice': [{'to': 'start', 'message': '다시 처음으로'}, {'to': 'end', 'message': '이어서 ㄱ'}]},
+    {'block': {'name': 'end'}},
+    {'talk': {'talker': '', 'message': '근데 할말이 없네 다시 처음으로 ㄱ'}},
+    {'jump': {'to': 'start'}}
 ];
 
 export default test;
