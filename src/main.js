e9517bdf5a5659d5199312f5eb38ed7741304399
KimDeokwon <codevillain@naver.com>
2018-12-21 00:54:20 +0900

    Update main.js
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 "use strict";
 
-// var engine = require('./engine');
 import { Engine } from "./engine.js"
-import script from './scripts.js'
+import { test, test2 } from './scripts.js'
 import setting from './settings.js'
 
-console.log(script);
-console.log(setting);
-
-let engine = new Engine(script, setting);
+let engine = new Engine(test, setting);
 engine.start();
