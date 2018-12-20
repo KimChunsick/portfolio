f751767366972ac38f0b00b1e6cabe579f78bff0
KimDeokwon <codevillain@naver.com>
2018-12-21 01:01:31 +0900

    Update srcipts.js
    Signed-off-by: KimDeokwon <codevillain@naver.com>

--- src/scripts.js
+++ src/scripts.js
@@ -2,10 +2,10 @@
 
 const test = [
     {'block': {'name': 'start'}},
-    {'bgm': {'filename': 'Zombie_March.mp3'}},
     {'talk': {'talker': '테스트', 'message': '테스트입니다.테스트입니다.테스트입니다.테스트입니다.테스트입니다.'}},
-    {'sfx': {'filename': 'shooting_star.mp3'}},
+    {'bgm': {'filename': 'Zombie_March.mp3'}},
     {'talk': {'talker': '마을사람A', 'message': '지나가는중...'}},
+    {'sfx': {'filename': 'shooting_star.mp3'}},
     {'add': {'message': '   이건 추가다아아아아아아아아아아ㅏ아아아아아아앙'}},
     {'choice': [{'message': '다시 처음으로', 'to': 'start'}, {'message': '이어서 ㄱ', 'to': 'end'}]},
     {'block': {'name': 'end'}},
