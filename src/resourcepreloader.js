8b62e35ff90333dae9c0504c8c737c3bb7e374e0
KimDeokwon <codevillain@naver.com>
2018-12-22 01:14:31 +0900

    Add resourcepreloader.js
    Signed-off-by: KimDeokwon <codevillain@naver.com>

---
+++ src/resourcepreloader.js
@@ -0,0 +1,73 @@
+"use strict";
+
+class ResourcePreloader {
+    constructor() {
+        this.targets = [
+            'assets/images/share.png',
+            'assets/sounds/bgm/Zombie_March.mp3',
+            'assets/sounds/sfx/shooting_star.mp3',
+        ];
+
+        this.preloadPromises = [];
+        this.progress = document.getElementById('loading_progress');
+        this.progress.setAttribute('max', this.targets.length.toString());
+    }
+
+    preload(callback) {
+        for (let i in this.targets) {
+            let path = this.targets[i];
+            // console.log(typeof target);
+            if (path.includes('sounds')) {
+                console.log(path);
+                this.preloadPromises.push(this.loadAudio(path));
+            } else if (path.includes('images')) {
+                console.log(path);
+                this.preloadPromises.push(this.loadImage(path));
+            }
+        }
+
+        const self = this;
+        Promise.all(this.preloadPromises).then(function () {
+            console.log('로딩 종료.');
+            self.progress.hidden = true;
+            callback();
+        }).catch (function (error) {
+            console.error(error);
+        });
+    }
+
+    loadImage(path) {
+        const self = this;
+        return new Promise(function (resolve, reject) {
+            const image = new Image();
+            image.onload  = function () {
+                const currentValue = parseInt(self.progress.value);
+                self.progress.value = (currentValue + 1);
+                resolve();
+            };
+            image.onerror = function (error) {
+                const currentValue = parseInt(self.progress.value);
+                self.progress.value = (currentValue + 1);
+                reject(error);
+            };
+            image.src = path;
+        });
+    }
+
+    loadAudio(path) {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+            request.open('GET', path);
+            request.onload = function() {
+                resolve();
+            };
+            request.onerror = function(error) {
+                console.log(error);
+                reject(error);
+            };
+            request.send();
+        });
+    }
+}
+
+export default ResourcePreloader;
