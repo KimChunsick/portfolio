eaa3cf79330a1cc9f9067c9a2b24605b48db43b8
BangChunDeok <BangChunDeok@users.noreply.github.com>
2020-08-23 13:55:48 +0000

    deploy: 1ac3b42fba07933370ebe232b3a420c76c9d2246

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import '@/style/global.css'
-import InGameScene from '@/scenes/InGameScene'
-
-const app = document.getElementById('app')
-const inGameScene = new InGameScene()
-app.insertAdjacentHTML('afterbegin', inGameScene.render())
-inGameScene.mount()
